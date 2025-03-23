370f18616e2c903ec218dd4b7fcfd9de33405602
s30623 <s30623@pjwstk.edu.pl>
2025-03-23 13:08:26 +0100

    .

--- Cwiczenia3/KontenerChlodniczy.cs
+++ Cwiczenia3/KontenerChlodniczy.cs
@@ -10,11 +10,33 @@ namespace Cwiczenia3
     {
         public string _rodzaj_produktu {  get; set; }
         private float _temperatura_kontenera {  get; set; }
-        private List<string> _produkty {  get; set; }
-        public KontenerChlodniczy(float masa_ladunku, float wysokosc, float waga_wlasna, float glebokosc, float maksymalna_ladownosc) : base(masa_ladunku, wysokosc, waga_wlasna, glebokosc, maksymalna_ladownosc)
+        private ICollection<Produkt> _produkty {  get; set; }
+        public KontenerChlodniczy(string rodzaj_produktu, float masa_ladunku, float wysokosc, float waga_wlasna, float glebokosc, float maksymalna_ladownosc) : base(masa_ladunku, wysokosc, waga_wlasna, glebokosc, maksymalna_ladownosc)
         {
+            this._rodzaj_produktu = rodzaj_produktu;
             this._rodzaj = 'C';
             Kontener.generujNumerSeryjny(this);
+        }
+        public void oproznijLadunek()
+        {
+            base.oproznijLadunek();
+            _produkty.Clear();
+        }
+        public virtual void zaladujKontener(Produkt produkt)
+        {
+            if (produkt._nazwa_produktu.Equals(this._rodzaj_produktu))
+            {
+                if(_temperatura_kontenera < produkt._temperatura_wymagana)
+                {
+                    throw new Exception("Za niska temperatura kontenera");
+                }
+                _masa_ladunku += produkt._masa;
+                if (_masa_ladunku > _maksymalna_ladownosc)
+                {
+                    throw new OverfillException();
+                }
+            }
+            
         }
 
     }
---
+++ Cwiczenia3/Produkt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenia3
+{
+    public class Produkt
+    {
+        public string _nazwa_produktu {  get; set; }
+        public float _temperatura_wymagana {  get; set; }
+        public float _masa {  get; set; }
+
+    }
+}
